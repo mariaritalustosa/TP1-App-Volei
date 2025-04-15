b3ff7f5d8e418e5eabdcb902b36f6b15797c63ef
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-04-15 09:28:12 -0300

    identação completa

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class PaginaHome extends StatelessWidget {
   const PaginaHome({super.key});
 
   final Color backgroundColor = const Color.fromARGB(255, 28, 185, 212);
-  final Color boxColor = const Color.fromARGB(255, 62, 214, 214);  
+  final Color boxColor = const Color.fromARGB(255, 62, 214, 214);
   final Color buttonColor = const Color.fromARGB(255, 12, 70, 158);
 
   @override
@@ -31,8 +31,8 @@ class PaginaHome extends StatelessWidget {
         onPressed: () {},
         backgroundColor: buttonColor,
         foregroundColor: Colors.white,
-        shape: RoundedRectangleBorder(borderRadius: 
-        BorderRadius.circular(50),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50),
         ),
         child: const Icon(Icons.add),
       ),
@@ -77,147 +77,140 @@ class PaginaHome extends StatelessWidget {
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
-                children: [ 
-                Row(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 150), 
-                    child: Align(
-                      alignment: Alignment.topLeft, 
-                      child: Container(
-                        width: 70,
-                        height: 220,
-                        decoration: BoxDecoration(
-                          color: boxColor,
-                          border: Border.all(
-                            color: const Color.fromARGB(255, 78, 226, 231),
-                            width: 3,
-                          ),
-                        ),
-                        child: Center(
-                          child: RotatedBox(
-                            quarterTurns: -1,
-                            child: Text(
-                              'TIMES',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 50,
-                                fontFamily: 'ConcertOne',
-                                letterSpacing: 2,
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 150),
+                        child: Align(
+                          alignment: Alignment.topLeft,
+                          child: Container(
+                            width: 70,
+                            height: 220,
+                            decoration: BoxDecoration(
+                              color: boxColor,
+                              border: Border.all(
+                                color: const Color.fromARGB(255, 78, 226, 231),
+                                width: 3,
+                              ),
+                            ),
+                            child: Center(
+                              child: RotatedBox(
+                                quarterTurns: -1,
+                                child: Text(
+                                  'TIMES',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 50,
+                                    fontFamily: 'ConcertOne',
+                                    letterSpacing: 2,
+                                  ),
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: const[
-                      Text(
-                        'SICRANOS',
-                        style: Styles.nomeTimes,
-                      ),
-                      SizedBox( width: 5),
-                      Text(
-                        '3',
-                        style: Styles.qtdJogadores,
-                      ),
-                      SizedBox(height: 5),
-                      RotatedBox(quarterTurns: 3,
-                      child: Text(
-                        'Jogadores',
-                        style: Styles.jogadores,
-                    ),  
-                      )
                     ],
                   ),
-                  const SizedBox(height: 5),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: const[
-                      Text(
-                        'AUTOCONVIDADOS',
-                        style: Styles.nomeTimes,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: const [
+                          Text(
+                            'SICRANOS',
+                            style: Styles.nomeTimes,
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            '3',
+                            style: Styles.qtdJogadores,
+                          ),
+                          SizedBox(height: 5),
+                          RotatedBox(
+                            quarterTurns: 3,
+                            child: Text(
+                              'Jogadores',
+                              style: Styles.jogadores,
+                            ),
+                          )
+                        ],
                       ),
-                      SizedBox(width: 5),
-                      Text(
-                        '3',
-                        style: Styles.qtdJogadores,
+                      const SizedBox(height: 5),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: const [
+                          Text(
+                            'AUTOCONVIDADOS',
+                            style: Styles.nomeTimes,
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            '3',
+                            style: Styles.qtdJogadores,
+                          ),
+                          SizedBox(width: 10),
+                          RotatedBox(
+                            quarterTurns: 3,
+                            child: Text(
+                              'Jogadores',
+                              style: Styles.jogadores,
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(width: 10),
-                      RotatedBox(quarterTurns: 3,
-                      child: Text(
-                        'Jogadores',
-                        style: Styles.jogadores,
-                      ),),
+                      const SizedBox(height: 5),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: const [
+                          Text(
+                            'ZIRALDOS',
+                            style: Styles.nomeTimes,
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            '4',
+                            style: Styles.qtdJogadores,
+                          ),
+                          SizedBox(width: 10),
+                          RotatedBox(
+                            quarterTurns: 3,
+                            child: Text(
+                              'Jogadores',
+                              style: Styles.jogadores,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 5),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: const [
+                          Text(
+                            'SPARRINGS',
+                            style: Styles.nomeTimes,
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            '5',
+                            style: Styles.qtdJogadores,
+                          ),
+                          SizedBox(height: 5),
+                          RotatedBox(
+                            quarterTurns: 3,
+                            child: Text(
+                              'Jogadores',
+                              style: Styles.jogadores,
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
-                  const SizedBox(height: 5),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: const[
-                      Text(
-                        'ZIRALDOS',
-                        style: Styles.nomeTimes,
-                      ),
-                      SizedBox(width: 5),
-                      Text(
-                        '4',
-                        style: Styles.qtdJogadores,
-                      ),
-                      SizedBox(width: 10),
-                      RotatedBox(quarterTurns: 3,
-                      child: Text(
-                        'Jogadores',
-                        style: Styles.jogadores,
-                      ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 5),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                  children: const [
-                    Text(
-                      'SPARRINGS',
-                      style: Styles.nomeTimes,
-                    ),
-                    SizedBox(width: 5),
-                    Text(
-                      '5',
-                      style: Styles.qtdJogadores,
-                    ),
-                    SizedBox(height: 5),
-                    RotatedBox(quarterTurns: 3,
-                    child: Text(
-                      'Jogadores',
-                      style: Styles.jogadores,
-                    ),),
-                  ],),
                 ],
-              ),
-              ],
-              ),
-
-             
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-           
-              
-                
-                 
-                ],
-              ),
               ),
             ],
           ),
@@ -226,12 +219,13 @@ class PaginaHome extends StatelessWidget {
     );
   }
 }
-class Cores{
+
+class Cores {
   static const corAzul = Color.fromARGB(255, 12, 70, 158);
   static const amarelo = Colors.amberAccent;
 }
 
-class Styles{
+class Styles {
   static const TextStyle nomeTimes = TextStyle(
     fontSize: 30,
     fontFamily: 'ConcertOne',
