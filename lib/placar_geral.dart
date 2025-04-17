0085e55a4ff3f98030d514ff65ed208608a194fd
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-04-17 14:19:59 -0300

    feat: cria tela PlacarGeral com orientação horizontal e definindo já seu background

--- lib/placar_geral.dart
+++ lib/placar_geral.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class PlacarGeral extends StatelessWidget{
     const PlacarGeral({super.key});
 
-
     @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+    Widget build(BuildContext context) {
+      WidgetsFlutterBinding.ensureInitialized();
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.landscapeLeft
+        ]);
+        return Scaffold(
         backgroundColor: Color.fromARGB(255, 28, 185, 212),
     );
   }
