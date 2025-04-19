c4395ab6809c0914552d5d0f1d5a9281aca5a4a0
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-04-19 11:01:25 -0300

    cria a classe Botoes de ação flutuantes em duas colunas, nas extremidades da tela

--- lib/placar_geral.dart
+++ lib/placar_geral.dart
@@ -12,6 +12,25 @@ class PlacarGeral extends StatelessWidget{
         ]);
         return Scaffold(
         backgroundColor: Color.fromARGB(255, 28, 185, 212),
+        body: const ColunasPrincipais(),
     );
+  }
+}
+
+
+class Botoes extends StatelessWidget{
+  const Botoes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(onPressed: (){
+      },
+      backgroundColor: Color.fromARGB(255, 12, 70, 158),
+      foregroundColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: const Icon(Icons.add),
+      );
   }
 }
