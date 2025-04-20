ae263d261484ce78f24fa86a37627d9c493481b8
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-04-20 12:54:02 -0300

    adição da fontFamily, aumento da fonte e aumento do espaço entre os botões

--- lib/placar_geral.dart
+++ lib/placar_geral.dart
@@ -19,8 +19,9 @@ class PlacarGeral extends StatelessWidget{
 
 class ColunasPrincipais extends StatelessWidget {
   const ColunasPrincipais({super.key});
+
   @override
-   Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 40),
       child: Row(
@@ -29,24 +30,55 @@ class ColunasPrincipais extends StatelessWidget {
           //coluna esquerda
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(4, (_) => const Botoes())
-            .expand((botao) => [botao, const SizedBox(height: 8)])
-            .toList()
-            ..removeLast(),
-            ),
-            //coluna direita
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: List.generate(4, (index) {
+              //lista de palavras para a coluna esquerda
+              List<String> textoColunaEsquerda = ['ACE', 'ATAQUE', 'BLOQUEIO', 'ERRO'];
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 6.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Botoes(),
+                    const SizedBox(width: 13),
+                    Text(
+                      textoColunaEsquerda[index], 
+                      style: const TextStyle(fontSize: 25, color: Colors.white, fontFamily: 'ConcertOne'),
+                    ),
+                  ],
+                ),
+              );
+            }),
+          ),
+          //coluna direita
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(4, (_) => const Botoes())
-            .expand((botao) => [botao, const SizedBox(height: 8)])
-            .toList()
-            ..removeLast(),
-            ),
-          ],
-        ),
-      );
-    }
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: List.generate(4, (index) {
+              // Lista de palavras para a coluna direita
+              List<String> textoColunaDireita = ['ACE', 'ATAQUE', 'BLOQUEIO', 'ERRO'];
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 6.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      textoColunaDireita[index], 
+                      style: const TextStyle(fontSize: 25, color: Colors.white, fontFamily: 'ConcertOne'),
+                    ),
+                    const SizedBox(width: 13),
+                    const Botoes(),
+                  ],
+                ),
+              );
+            }),
+          ),
+        ],
+      ),
+    );
+  }
 }
+
 
 class Botoes extends StatelessWidget{
   const Botoes({super.key});
@@ -64,3 +96,4 @@ class Botoes extends StatelessWidget{
       );
   }
 }
+
