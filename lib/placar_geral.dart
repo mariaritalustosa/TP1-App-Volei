3d8229c83ca638bd68c22f39a707dbebf03b47bb
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-04-28 19:53:14 -0300

    cria um container com layout com o nome dos times e em seguida sua respectiva pontuação

--- lib/placar_geral.dart
+++ lib/placar_geral.dart
@@ -43,6 +43,71 @@ class PlacarGeral extends StatelessWidget {
                 color: Color.fromARGB(244, 12, 70, 158),
               ),
             ),
+          ),
+          Container(
+            width: 460,
+            height: 250,
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 255, 126, 52),
+              border: Border.all(color: Colors.white, width: 4),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Color.fromARGB(255, 179, 246, 248),
+                      child: Text(
+                        'A', 
+                        style: TextStyle(
+                          fontFamily: 'ConcertOne',
+                          fontSize: 20,
+                          color: Color.fromARGB(255, 12, 70, 158),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    ...[
+                      {'time': 'Ziraldos', 'pontos': 25},
+                      {'time': 'Ziraldos', 'pontos': 25},
+                      {'time': 'Ziraldos', 'pontos': 10},
+                      {'time': 'Sparrings', 'pontos': 25},
+                    ].map((Map<String, dynamic> times) => Padding(
+                    padding: 
+                    EdgeInsets.symmetric(vertical: 4),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(child: Text(
+                          times['time'],
+                          style: TextStyle(
+                            fontFamily: 'ConcertOne',
+                            fontSize: 15,
+                            color: Colors.white,
+                          ),
+                        ),
+                        ),
+                        Text(
+                          '${times['pontos']}',
+                          style: TextStyle(
+                            fontFamily: 'ConcertOne',
+                            fontSize: 20,
+                            color: times['pontos'] == 25 ? Color.fromARGB(255, 12, 70, 158) : Colors.amber,
+                          ),
+                        ),
+                      ],
+                    ),
+                    )),
+                  ],
+                ),
+                ),
+                Container()
+              ],
+            ),
           )
         ],
       ),
